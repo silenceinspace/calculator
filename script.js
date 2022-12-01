4dfb0cbffbfd5e76271d5938b9d7796a7270e629
Anton <anton20020426@gmail.com>
2022-12-01 12:30:31 +0100

    Start doing a basic html calculator

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('lets begin!');
+//function expression vs function declaration;
+function add(a,b) {
+    return a+b;
+};
+
+function subtract(a,b) {
+    return a-b;
+} 
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(num1, num2, operator){
+    if(operator == '+') {
+        return result = add(num1, num2);
+    }
+
+    if(operator == '-') {
+        return result = subtract(num1, num2);
+    }
+
+    if(operator == '*') {
+        return result = multiply(num1, num2);
+    }
+
+    if(operator == '/') {
+        return result = divide(num1, num2);
+    };
+};
+
