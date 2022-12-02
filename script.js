5a5c534c18108759532149baddad9e5a90af131d
Anton <anton20020426@gmail.com>
2022-12-02 18:08:03 +0100

    Values of buttons reflect input on the display

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-//function expression vs function declaration;
+//function expression vs function declaration??;
 function add(a,b) {
     return a+b;
 };
-
 function subtract(a,b) {
     return a-b;
-} 
-
+};
 function multiply(a,b) {
     return a*b;
-}
-
+};
 function divide(a,b) {
     return a/b;
-}
+};
 
 function operate(num1, num2, operator){
     if(operator == '+') {
@@ -33,25 +30,61 @@ function operate(num1, num2, operator){
     };
 };
 
-const calculatorCase = document.querySelector('.digits');
-function createButtons() {
-    for (let i=9; i>= 0; i--) {
-        let button = document.createElement('button');
-        button.className = 'item';
-        button.textContent = `${i}`;
-        calculatorCase.appendChild(button);
-    }
-};
-createButtons();
+//make the calculator work!
+const divisionKey = document.querySelector('.division');
+const multiplicationKey = document.querySelector('.multiplication');
+const plusKey = document.querySelector('.plus');
+const minusKey = document.querySelector('.minus');
 
-const buttons = document.querySelectorAll('.item');
-buttons.forEach((item) => {
-    item.addEventListener('click', populateDisplay);
-})
+plusKey.addEventListener('click', () => {
+    display.textContent += plusKey.value;
+    saveFirstNumber(); //test
+    return '+';
+});
+minusKey.addEventListener('click', () => {
+    display.textContent += minusKey.value;
+});
+divisionKey.addEventListener('click', () => {
+    display.textContent += divisionKey.value;
+});
+multiplicationKey.addEventListener('click', () => {
+    display.textContent += multiplicationKey.value;
+});
 
-const display = document.querySelector('.display-values');
-function populateDisplay() {
-    display.textContent += '5';
+//save the fist number + chosen operator? 
+let num1;
+let num2;
+function saveFirstNumber() {
+    num1 = currentValue;
 }
 
-//find a way how to populate the display with the exact button I press (e.g. if I press "1", then the display should be populated with "1");
+
+const calculatorCase = document.querySelector('.digits');
+(function createButtons() {
+    for (let i=9; i>= 0; i--) {
+        let button = document.createElement('button');
+        button.className = 'item number';
+        button.textContent = `${i}`;
+        button.value = `${i}`;
+        calculatorCase.appendChild(button);
+    }
+}()); //immediate function
+
+const buttons = document.querySelectorAll('.number');
+const display = document.querySelector('.display-values');
+const clearKey = document.querySelector('.clear-key');
+
+// let valueInTheDisplay
+let currentValue = 0;
+buttons.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        display.textContent += `${e.target.value}`;
+        currentValue = display.textContent;
+    });
+})
+
+clearKey.addEventListener('click', () => {
+    display.textContent = '';
+    currentValue = 0;
+});
+
